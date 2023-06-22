a3db026eca9979d61cff30390c710538d965d60f
Bot-on-Tapwater <mundabrandon@outlook.com>
2023-06-22 18:14:10 +0300

    Bubble Sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: Array of integers
+ * @size: The size of array of integers
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+	int count = 0;
+	int swap = 0;
+
+	if (size == 2)
+	{
+		if (array[0] > array[1])
+		{
+			temp = array[0];
+			array[0] = array[1];
+			array[1] = temp;
+			print_array(array, size);
+		}
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		/* Iterate through the entire list*/
+		{
+			for (j = 1; j < (size - count); j++)
+			/*It gets us the next element to compare array[0] with*/
+			{
+				if (array[j - 1] > array[j])
+				{
+					temp = array[j - 1];
+					array[j - 1] = array[j];
+					array[j] = temp;
+					print_array(array, size);
+					swap = 1;
+				}
+			}
+			count++;
+			if (swap == 0)
+			{
+				return;
+			}
+		}
+		printf("\t\nNo. of loops :%d\n", loop);
+		}
+}
---
+++ testing.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+#bubble
+
+if __name__ == '__main__':
+
+	# Our unsorted list
+	unsorted_list = [19, 48, 99, 71, 13, 52, 96, 73, 86, 7]
+
+	# swap helps us know if our list is already sorted in ascending order
+	swap = 1
+	count = 1
+	loop = 1
+
+	while(swap == 1):
+		# initiate swap to zero
+		swap = 0
+
+		# loop through indices of our unsorted list
+		for i in range(len(unsorted_list) - count):			
+			try:
+				# check if 
+				if (unsorted_list[i] > unsorted_list[i + 1]):
+					unsorted_list[i], unsorted_list[i + 1] = unsorted_list[i + 1], unsorted_list[i]
+					swap = 1
+					print(f"\t{unsorted_list}")
+			
+			except IndexError:
+				pass
+				
+			finally:
+				loop += 1		
+		count += 1
+
+	print(loop)
